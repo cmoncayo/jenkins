9d43d826eff18195d80043a763c309b4f48a460b
cmoncayo <cmoncayo@espol.edu.ec>
2018-01-03 11:50:13 -0500

    cambio hecho

--- personas.php
+++ personas.php
@@ -1,12 +1,12 @@
 <?php
-class Personas{
+class personas{
     protected $nombre;
 public function getNombre(){
 	return $this->nombre;
 	}
 public function setNombre($nombre){
 	$this->nombre 0 $nombre;
-public function talk(){
+public function talk() {
 	return "feliz año nuevo";
 	}
     }
--- phptest.php
+++ phptest.php
@@ -1,10 +1,10 @@
 <?php
 require_once "PHPUnit/Autoload.php";
-require_once "php.php";
+require_once "personas.php";
 class phptest extends PHPUnit_Framework_TestCase
 {
 public function  escribir(){
-	$persona = new php();
+	$persona = new personas();
 	$expected = "feliz año viejo";
 	$actual = $persona->talk();
 	$this->asserEquals($expected, $actual);
