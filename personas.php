f362e668bc6adf443c0eacc26d9c6766cbd9c776
cmoncayo <33964816+cmoncayo@users.noreply.github.com>
2018-01-04 12:43:52 -0500

    Update personas.php
    hecho

--- personas.php
+++ personas.php
@@ -8,7 +8,7 @@ public function setNombre($nombre){
 	$this->nombre 0 $nombre;
 public function talk() {
 <<<<<<< HEAD
-	return "feliz año viejo otra vez ";
+	return "feliz año viejo otra ";
 =======
 	return "feliz año nuevo";
 >>>>>>> 9092ecc8375bc2bceb9a648f07de54eb54f1024d
